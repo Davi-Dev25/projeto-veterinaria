ad81011f0a90d28b719be5d42cee5cbe865115d6
Davi-Dev25 <oliveirasousadavi58@gmail.com>
2025-04-01 13:01:19 -0300

    ajeitando algumas coisas

---
+++ index.js
@@ -0,0 +1,12 @@
+function clickMenu() {
+    if (itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else{
+        itens.style.display = 'block'
+    }
+}
+let elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida){
+    duvida.addEventListener
+})
